9b52330f4bf1829e49399a685d81cb4221355348
c0der.br1t <c0der.br1t@gmail.com>
2019-12-03 10:16:49 -0600

    added content for each text block h4 and p. added contact information

--- js/index.js
+++ js/index.js
@@ -60,11 +60,81 @@ button.textContent = siteContent['cta']['button'];
 let middleImg = document.querySelector('#middle-img');
 middleImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
 
-let topText = document.querySelectorAll('.top-content > .text-content')
-console.log(topText);
+// let features = document.querySelector('div:nth-child(1) h4');
+// features.textContent = siteContent['main-content']['features-h4'];
 
-// topText.forEach((link, index) => {
+// let featuresText = document.querySelector('div:nth-child(1) p');
+// featuresText.textContent = siteContent['main-content']['features-content'];
 
+// let about = document.querySelector('div:nth-child(2) h4');
+// about.textContent = siteContent['main-content']['about-h4'];
+
+// let aboutText = document.querySelector('div:nth-child(2) p');
+// aboutText.textContent = siteContent['main-content']['about-content'];
+
+// let services = document.querySelector('div:nth-child(3) h4');
+// services.textContent = siteContent['main-content']['services-h4'];
+
+// let servicesText = document.querySelector('div:nth-child(3) p');
+// servicesText.textContent = siteContent['main-content']['services-content'];
+
+// let product = document.querySelector('div:nth-child(4) h4');
+// product.textContent = siteContent['main-content']['product-h4'];
+
+// let productText = document.querySelector('div:nth-child(4) p');
+// productText.textContent = siteContent['main-content']['product-content'];
+
+// let vision = document.querySelector('div:nth-child(5) h4');
+// vision.textContent = siteContent['main-content']['vision-h4'];
+
+// let visionText = document.querySelector('div:nth-child(5) p');
+// visionText.textContent = siteContent['main-content']['vision-content'];
+
+let headers = document.querySelectorAll('h4');
+headers[0].textContent = siteContent['main-content']['features-h4'];
+headers[1].textContent = siteContent['main-content']['about-h4'];
+headers[2].textContent = siteContent['main-content']['services-h4'];
+headers[3].textContent = siteContent['main-content']['product-h4'];
+headers[4].textContent = siteContent['main-content']['vision-h4'];
+headers[5].textContent = siteContent['contact']['contact-h4'];
+
+
+let paragraphs = document.querySelectorAll('p'); 
+paragraphs[0].textContent = siteContent['main-content']['features-content']; 
+paragraphs[1].textContent = siteContent['main-content']['about-content']; 
+paragraphs[2].textContent = siteContent['main-content']['services-content'];
+paragraphs[3].textContent = siteContent['main-content']['product-content'];
+paragraphs[4].textContent = siteContent['main-content']['vision-content'];
+paragraphs[5].textContent = siteContent['contact']['address'];
+paragraphs[6].textContent = siteContent['contact']['phone'];
+paragraphs[7].textContent = siteContent['contact']['email'];
+// let topText = document.querySelectorAll('.top-content > .text-content')
+// console.log(topText);
+
+// let headers = document.querySelectorAll('h4:nth-of-type(1)');
+// console.log(headers);
+
+// headers.forEach((link, index) => {
+//   link.textContent = siteContent['main-content'][index];
 // })
+
+// featuresHead.textContent = siteContent['main-content']['features-h4'];
+
+// console.log(featuresHead);
+
+// let aboutHead = document.querySelectorAll('.top-content .text-content:last-child > h4');
+
+// aboutHead.textContent = siteContent['main-content']['about-h4'];
+
+// console.log(aboutHead);
+
+// let featuresText = document.querySelectorAll('.top-content > .text-content:first-child p');
+
+// featuresText.textContent = siteContent['main-content']['features-content'];
+
+// let aboutText = document.querySelectorAll('.top-content > .text-content:last-child p');
+
+// aboutText.textContent = siteContent['main-content']['about-content'];
+
 
 
